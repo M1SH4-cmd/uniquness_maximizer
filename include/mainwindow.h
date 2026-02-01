f3201a91c6d1088cf239db5e827290ab01d48e8c
OG_Michael <6el0zu61979@mail.ru>
2026-02-01 19:44:11 +0700

    dropped mainwin

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -17,7 +17,7 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr, std::string path = "");
+    //MainWindow(QWidget *parent = nullptr, std::string path = "");
     ~MainWindow();
 
 
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+//#include "mainwindow.h"
 #include "maximizer.h"
 
 MainWindow::MainWindow(QWidget *parent, std::string path)
