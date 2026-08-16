9e6e67724dc29159dee7309a3ca157c881a221b2
OG_Michael <6el0zu61979@mail.ru>
2026-08-16 23:49:48 +0700

    deleted shit

---
+++ build/.qt/QtDeployTargets-RelWithDebInfo.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_uniquness_maximizer_FILE C:/Programming/Development/Projs/uniquness_maximizer/build/RelWithDebInfo/uniquness_maximizer.exe)
+set(__QT_DEPLOY_TARGET_uniquness_maximizer_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_uniquness_maximizer_RUNTIME_DLLS C:/Qt/6.9.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Core.dll;C:/vcpkg/installed/x64-windows/bin/zip.dll;C:/vcpkg/installed/x64-windows/bin/pugixml.dll)
---
+++ build/Desktop_Qt_6_10_1_MSVC2022_64bit-Debug/CMakeFiles/uniquness_maximizer.dir/resources/icon.i
@@ -0,0 +1,2 @@
+#line 1 "C:\\Projs\\uniq_mxzr\\build\\Desktop_Qt_6_10_1_MSVC2022_64bit-Debug\\resources\\icon.rc"
+IDI_ICON1 ICON "C:/Projs/uniq_mxzr/resources/icon.ico"
